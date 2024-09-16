1a8b1e00793a19eb823039e2da63ee918e89de29
Asif <asiframzan2021@gmail.com>
2024-09-16 12:59:41 +0500

    show password option added to login page

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -5,10 +5,12 @@ import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  EyeIcon,
+  EyeSlashIcon
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import { useActionState } from 'react';
+import { useActionState, useState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
@@ -16,6 +18,8 @@ export default function LoginForm() {
     authenticate,
     undefined,
   );
+  const [show, setShow] = useState(true);
+
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -53,14 +57,15 @@ export default function LoginForm() {
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
-                type="password"
+                type={show && "password" || "text"}
                 name="password"
                 placeholder="Enter password"
                 required
                 minLength={6}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
+              {show ? <EyeIcon onClick={()=>{setShow(!show)}} className='absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900'/> : <EyeSlashIcon onClick={()=>{setShow(!show)}} className='absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900'/>}
+              </div>
           </div>
         </div>
         <Button className="mt-4 w-full" aria-disabled={isPending}>
